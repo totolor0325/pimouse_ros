969529bcbbbd6f07f26bf27763ba9fea2f0a484d
David Chuang <totolor0325@gmail.com>
2019-09-18 15:39:16 +0800

    Update travis_ros_install.bash

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,11 +1,11 @@
 #!/bin/bash -xve
 
 #required packages
-pip install -U pip
-pip install catkin_pkg
-pip install empy
-pip install pyyaml
-pip install rospkg
+sudo pip install -U pip
+sudo pip install catkin_pkg
+sudo pip install empy
+sudo pip install pyyaml
+sudo pip install rospkg
 
 #ros install
 cd ..
